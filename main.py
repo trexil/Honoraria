145bdd866f613d708164d583c8099df2d04b646c
trexil <54093159+trexil@users.noreply.github.com>
2022-01-02 15:57:03 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,156 @@
+from pywebio import *
+from pywebio.utils import pyinstaller_datas
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import *
+from pywebio.pin import *
+from pathlib import Path
+from openpyxl import Workbook
+from winreg import *
+import os
+from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
+import warnings
+import argparse
+from pywebio.exceptions import SessionClosedException
+from pywebio import start_server
+
+warnings.filterwarnings("ignore")
+
+
+with OpenKey(HKEY_CURRENT_USER, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders') as key:
+   Downloads = QueryValueEx(key, '{374DE290-123F-4565-9164-39C4925E467B}')[0]
+
+
+def main():
+    data = input_group("Thesis/Design Information",[
+        input('Title of Thesis/Design', name='title'),
+        checkbox('Advisor', options=['Caya, Meo Vincent ', 'Hortinela, Carlos IV', 'Linsangan, Noel', 'Manlises, Cyrel', 'Maramba, Rafael', 'Pellegrino, Rosemarie', 'Torres, Jumelyn', 'Villaverde, Jocelyn', 'Yumang, Analyn'], name='advisor'),
+        checkbox('Panels', options=['Caya, Meo Vincent ', 'Hortinela, Carlos IV', 'Linsangan, Noel', 'Manlises, Cyrel', 'Maramba, Rafael', 'Pellegrino, Rosemarie', 'Torres, Jumelyn', 'Villaverde, Jocelyn', 'Yumang, Analyn'], name='panels'),
+        input("Payor Name", name='payor'),
+        input("Reference Number", name='refnum'),
+    ])
+    
+    if not data['title']:
+        popup('Error', [
+            put_text('Missing Title')])
+        main()
+    elif not data['advisor']:
+        popup('Error', [
+            put_text('Missing Advisor')])
+        main()
+    elif not data['panels']:
+        popup('Error', [
+            put_text('Missing Panels')])
+        main()
+    elif not data['payor']:
+        popup('Error', [
+            put_text('Missing Payor')])
+        main()
+    elif not data['refnum']:
+        popup('Error', [
+            put_text('Missing Reference Number')])
+        main()
+
+    Title = data['title']
+    Advisor = data['advisor']
+    Panels = data['panels']
+    Payor = data['payor']
+    Ref = data['refnum']
+    column = len(Advisor) + len(Panels) + 4
+    Y = ["A", 'B', 'C', 'D', 'E', 'F']
+    filename = Title + ".xlsx"
+
+    wb =  Workbook()
+    ws = wb.active
+
+
+    ws.merge_cells(start_row=1, start_column=1, end_row=column, end_column=1)
+    ws['A1'].alignment = Alignment(horizontal='center', vertical='center')
+    ws.column_dimensions['A'].width = 4
+    ws['A1'] = 1
+
+
+    ws.merge_cells('B1:B2')
+    ws['B1'].alignment = Alignment(horizontal='center', vertical='center')
+    ws.column_dimensions['B'].width = 20
+    ws['B1']= "FACULTY NAME"
+    x = 3
+    for adv in Advisor:
+        ws['B'+ str(x)]=adv
+        ws['C'+ str(x)]="Advisor"
+        ws['E'+ str(x)]= "PHP 3,000"
+        ws['F'+ str(x)]= "ATM"
+        x+=1
+    y = len(Advisor) + 4
+    for pan in Panels:
+        ws['B'+ str(y)]=pan
+        ws['C'+ str(y)]="Panel"
+        ws['E'+ str(y)]="PHP 1,500"
+        ws['F'+ str(y)]="ATM"
+        y+=1
+
+    ws.merge_cells('C1:C2')
+    counterC=1
+    while counterC!=column:
+        ws['C'+str(counterC)].alignment = Alignment(vertical='center', horizontal='center', wrapText=True)
+        counterC+=1
+    ws.column_dimensions['C'].width = 10
+    ws['C1']= "ADVISOR or PANEL"
+
+    
+
+    ws.merge_cells('D1:D2')
+    counterD=1
+    while counterD<=column:
+        ws['D'+str(counterD)].alignment = Alignment(vertical='center', horizontal='center', wrapText=True)
+        counterD+=1
+    ws.merge_cells(start_row=3, start_column=4, end_row=column-2, end_column=4)
+    ws.column_dimensions['D'].width = 30
+    ws['D1'] = 'TITLE OF THESIS/DESIGN'
+    ws['D3'] = Title
+    ws['D' + str(column-1)] = Payor
+    ws['D' + str(column)] = Ref
+
+    ws.merge_cells('E1:E2')
+    counterE=1
+    while counterE<column:
+        ws['E'+str(counterE)].alignment = Alignment(vertical='center', horizontal='center')
+        counterE+=1
+    ws.column_dimensions['E'].width = len("HONORARIUM")+5
+    ws['E1']="HONORARIUM"
+
+    ws.merge_cells('F1:F2')
+    counterF=1
+    while counterF<column:
+        ws['F'+str(counterF)].alignment = Alignment(vertical='center', horizontal='center', wrapText=True)
+        counterF+=1
+    ws.column_dimensions['F'].width = 12
+    ws['F1']="MODE OF PAYMENT"
+
+    for C in Y:
+        for R in range(column):
+            ws[str(C)+str(R+1)].font=Font(name="Arial")
+            ws[str(C)+str(R+1)].border=Border(left=Side(border_style='thin',color='00000000'),right=Side(border_style='thin',color='00000000'), top=Side(border_style='thin',color='00000000'), bottom=Side(border_style='thin',color='00000000'))
+    wb.save(Downloads+'\\'+filename.strip())
+
+    return(data['title'],data['advisor'],data['panels'],data['payor'],data['refnum'])
+
+def process():
+    main()
+    termi = True
+    while termi:
+        put_text('Success')
+        keep_main = radio('Continue?', ['Yes', 'No'])
+        if keep_main == 'Yes':
+            main()
+        else:
+            put_text('Thank you!')
+            termi = False
+
+process()
+
+
+
+
+
+
