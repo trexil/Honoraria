c2183ee938736b46463cfe339e91fb7ee9952563
trexil <54093159+trexil@users.noreply.github.com>
2022-01-04 19:09:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import io
 from pywebio.exceptions import SessionClosedException
 from pywebio import start_server
 from flask import Flask, send_from_directory, send_file, make_response, request
-import flask.ext.excel
 from pywebio import STATIC_PATH
 from pywebio.platform.flask import webio_view
 from django.http import FileResponse
