14f87c5e866a2c7d0f3d86fc28626ef3ffbafc04
trexil <54093159+trexil@users.noreply.github.com>
2022-01-04 20:45:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def process():
             
 if __name__ == '__main__':
     app.run()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=8080)
-    args = parser.parse_args()    
-    start_server(process, port=args.port)
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument("-p", "--port", type=int, default=8080)
+    #args = parser.parse_args()    
+    #start_server(process, port=args.port)
