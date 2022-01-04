e59ff64da0c4d5c997e0ec30f39564ed9684db46
trexil <54093159+trexil@users.noreply.github.com>
2022-01-04 18:16:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -154,12 +154,10 @@ def process():
         else:
             put_text('Thank you!')
             termi = False
-
-process()
             
-#if __name__ == '__main__':
-#    parser = argparse.ArgumentParser()
-#    parser.add_argument("-p", "--port", type=int, default=8080)
-#    args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    args = parser.parse_args()
 
-#    start_server(process, port=args.port)
+    start_server(process, port=args.port)
