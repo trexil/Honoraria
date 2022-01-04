5e88633a3c7731e9376e1dc5013fd4d5e04d3147
trexil <54093159+trexil@users.noreply.github.com>
2022-01-04 19:04:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import argparse
 import io
 from pywebio.exceptions import SessionClosedException
 from pywebio import start_server
-from flask import Flask, send_from_directory, send_file, make_response, request, excel
+from flask import Flask, send_from_directory, send_file, make_response, request
 import flask_excel as excel
 from pywebio import STATIC_PATH
 from pywebio.platform.flask import webio_view
